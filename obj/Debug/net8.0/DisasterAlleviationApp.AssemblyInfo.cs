3b694bbd53e87e7f6d9cdafa1bc8ca6d8b4e2b18
Julie-portfolio <dimercia.nabi@gmail.com>
2025-11-03 22:32:00 +0200

    Initial commit of DisasterAlleviationFoundation project

--- obj/Debug/net8.0/DisasterAlleviationApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DisasterAlleviationApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisasterAlleviationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c570571d63744e6ccd8cb58eefd6e21f21d67f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DisasterAlleviationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisasterAlleviationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
